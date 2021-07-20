d2353f8aecad9a2dc8f701006a92b6e348edb9a1
Dora <dorababumuvvala@gmail.com>
2021-07-20 17:23:16 +0530

    Some stmts added

--- src/main/java/in/ashokit/ProctionApp.java
+++ src/main/java/in/ashokit/ProctionApp.java
@@ -3,6 +3,7 @@ package in.ashokit;
 public class ProctionApp {
 	public static void main(String[] args) {
 		System.out.println("My products ....!!");
+		System.out.println("Here more Offers");
 	}
 
 }
